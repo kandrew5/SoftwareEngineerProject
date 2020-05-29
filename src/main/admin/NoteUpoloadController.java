47e363540b7f792933bec94af5c19ee14c6bffd9
ThomasChatzopoulos <chatzothomas@gmail.com>
2020-05-29 19:18:42 +0300

    new decision & alert, export to excel, fxml controllers

---
+++ src/main/admin/NoteUpoloadController.java
@@ -0,0 +1,67 @@
+package main.admin;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NoteUpoloadController {
+    public void click_communication(MouseEvent mouseEvent) throws IOException {
+        Node node = (Node) mouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("recent_messages.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+
+        scene.setRoot(root);
+    }
+
+    public void click_calendar(MouseEvent mouseEvent) throws IOException {
+        Node node = (Node) mouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("calendar.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+
+        scene.setRoot(root);
+    }
+
+    public void click_Timetable(MouseEvent mouseEvent) throws IOException {
+        Node node = (Node) mouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("timetable.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+
+        scene.setRoot(root);
+    }
+
+    public void click_decisions(MouseEvent mouseEvent) throws IOException {
+        Node node = (Node) mouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("all_decisions.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+
+        scene.setRoot(root);
+    }
+
+    public void click_Votings(MouseEvent mouseEvent) throws IOException {
+        Node node = (Node) mouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Scene scene = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("newVoting.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+
+        scene.setRoot(root);
+    }
+}
